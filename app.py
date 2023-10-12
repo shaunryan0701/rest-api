550fc35252e51238cc824350c11fb408f10d85bb
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-10-12 10:42:16 +0100

    Uodated REST API

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
+import uuid
+
 from flask import Flask, request
+from flask_smorest import abort 
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-  {
-    "name": "My Store",
-    "items": [
-      {
-        "name": "Chair",
-        "price": 16.99
-      }
-    ]
-  }
-]
+
+# stores = [
+#   {
+#     "name": "My Store",
+#     "items": [
+#       {
+#         "name": "Chair",
+#         "price": 16.99
+#       }
+#     ]
+#   }
+# ]
 
 @app.route("/")
 def hello_world():
@@ -20,41 +25,45 @@ def hello_world():
 
 @app.get("/store")
 def get_stores():
-  return {"stores": stores}
+  return {"stores": list(stores.values())}
 
 @app.post("/store")
 def create_store():
-  request_data = request.get_json()
-  new_store = {"name": request_data["name"], "items": []}  
+  store_data = request.get_json()
+  store_id = uuid.uuid4().hex
+  store = {**store_data, "id": store_id} 
+  stores[store_id] = store
 
-  stores.append(new_store)
-  return new_store, 201
+  return store, 201
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-  request_data = request.get_json()
-  for store in stores:
-    if store["name"] == name:
-        new_item = {"name": request_data["name"], "price": request_data["price"]}
-        store["items"].append(new_item)
-        return new_item, 201
-    
-  return {"message": "Store not found" }, 404
-        
+@app.post("/item")
+def create_item():
+  item_data = request.get_json()
+  if item_data["store_id"] not in stores:
+     abort(404, message=f"Store {item_data['store_id']} not found")
+  
+  item_id = uuid.uuid4().hex
+  item = {**item_data, "id": item_id} 
+  items[item_id] = item
 
-@app.get("/store/<string:name>")
-def get_store(name):
-  for store in stores:
-    if store['name'] == name:
-        return store
+  return item, 201
 
-  return {"message": "Store not found"}, 404   
+@app.get("/item")
+def get_items():
+  return {"items": list(items.values())}    
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+  
+  try:
+    return stores[store_id]
+  except KeyError:
+    abort(404, message=f"Store {store_id} not found")   
   
 
-@app.get("/store/<string:name>/item")
-def get_store_item(name):
-  for store in stores:
-    if store['name'] == name:
-        return {"items": store["items"]}
-
-  return {"message": "Store not found"}, 404  
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+  try:
+    return items[item_id]
+  except KeyError:
+    abort(404, message=f"Item {item_id} not found")
