096c857a8fcffcba91e76bba1c71bf71c7e6e94f
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-10-24 20:02:59 +0100

    Updated item resource for REST API

--- resources/item.py
+++ resources/item.py
@@ -2,47 +2,47 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort 
-from schemas import ItemSchema, ItemSchemaUodate
+from schemas import ItemSchema, ItemUpdateSchema
 from models import ItemModel
 from db import db
 from sqlalchemy.exc import SQLAlchemyError
 
 blp = Blueprint("items", __name__, description="Operations on items")
 
-@blp.route("/item/<string:item_id>")
+@blp.route("/item/<int:item_id>")
 class Item(MethodView):
     
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-      item = ItemModel.quwry.get_or_404(item_id)
+      item = ItemModel.query.get_or_404(item_id)
       return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": f'items[item_id] deleted'}
-        except KeyError:
-            abort(404, message=f"Item {item_id} not found")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted"}
 
-    @blp.arguments(ItemSchemaUodate)
+    @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item_data = request.get_json()
-            
-        try:
-          item = items[item_id]
-          item |= item_data
+        item = ItemModel.query.get(item_id)
+        if item:
+            item_price = item_data["price"]
+            item_name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+        db.session.add(item)
+        db.session.commit()
 
-          return item
-        except KeyError:
-            abort(404, message=f"Item {item_id} not found")
+        return item
 
 @blp.route("/item")
 class ItemList(MethodView):
     
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
     
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
