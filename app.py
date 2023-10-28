482d18ba337400954b2e285fc9f148ac4736f14d
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-10-28 17:50:52 +0100

    Updated app for REST API

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
 from flask import Flask
 from flask_smorest import Api
-import os
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
+import secrets
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
-def create_app(db_url: None):
-  app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
 
-  app.config["PROPAGATE_EXCEPTIONS"] = True
-  app.config["API_TITLE"] = "Stores REST API"
-  app.config["API_VERSION"] = "v1"
-  app.config["OPENAPI_VERSION"] = "3.0.3"
-  app.config["OPENAPI_URL_PREFIX"] = "/"
-  app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-  app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-  app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_RUL", "sqlite:///data.db")
-  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    api = Api(app)
 
-  db.init_app(app)
+    app.config["JWT_SECRET_KEY"] = "460074764855456427819309585337132427"
+    jwt = JWTManager(app)
+    with app.app_context():
+        db.create_all()
 
-  api = Api(app)
-  
-  with app.app_context():
-    db.create_all()
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
-  api.register_blueprint(ItemBlueprint)
-  api.register_blueprint(StoreBlueprint)
-
-  return app
+    return app
