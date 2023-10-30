cd5f1d686433f22a688bcbedde38f3857cc1f9bc
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-10-30 14:38:49 +0000

    Updated app.py for REST API

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.json import jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -30,6 +31,28 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "460074764855456427819309585337132427"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):   
+        return jsonify({
+            "description": "The token has expired",
+            "error": "token_expired"
+        }, 401)
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            "description": "Signature verification failed",
+            "error": "invalid_token"
+        }, 401)
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            "description": "Request does not contain an access token",
+            "error": "authorization_required"
+        }, 401)
+    
     with app.app_context():
         db.create_all()
 
