5e29faa5dba714a2d1bcfa8b3c44d7da391ea44b
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-11-01 09:56:33 +0000

    Updated models for REST API

--- models/tag.py
+++ models/tag.py
@@ -2,8 +2,10 @@ from db import db
 
 class TagModel(db.Model):
     __tablename__ = 'tags'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     store_id = db.Column(db.String(), db.ForeignKey('stores.id'), nullable=False)
 
     store = db.relationship('StoreModel', back_populates='tags')
+    items = db.relationship('ItemModel', back_populates='tags', secondary='items_tags')
