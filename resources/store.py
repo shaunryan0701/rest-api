d70e27ccb6b338400a37e71b2aaf0e6d6bdf13f2
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-11-03 15:32:42 +0000

    Updated resources for REST API

--- resources/store.py
+++ resources/store.py
@@ -3,43 +3,48 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort 
 from schemas import StoreSchema
+from models import StoreModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
-@blp.route("/store/<string:store_id>")
+@blp.route("/store/<int:store_id>")
 class Store(MethodView):
     
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message=f"Store {store_id} not found") 
-
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+    
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": f'stores[store_id] deleted'}
-        except KeyError:
-            abort(404, message=f"Store {store_id} not found")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}
 
 @blp.route("/store")
 class StoreList(MethodView):
     
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
     
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
     def post(self, store_data):
 
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message="Store already exists")
+        store = StoreModel(**store_data)
 
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id} 
-        stores[store_id] = store
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists"
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the store")
 
         return store
